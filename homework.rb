7377b5a829cc89edf1c3f1e8a6ff3227b4ca5b48
Yaritza <Yaritza@users.noreply.github.com>
2013-09-25 22:16:47 -0400

    Add more excitement!

--- homework.rb
+++ homework.rb
@@ -1,3 +1,3 @@
 # For today's homework write an application
 # that simulates 99 bottles of beer on the wall in code.
-puts "welcome to the 99 bottles game"
+puts "Welcome to the 99 bottles game!!!!!!!"
