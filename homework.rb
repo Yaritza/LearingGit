d7606317893d418e2b8280d8d7307e0be07fbb71
Yaritza <Saturn@Yaris-MacBook-Pro.local>
2013-09-25 21:56:56 -0400

    added homework and solution files

---
+++ homework.rb
@@ -0,0 +1,3 @@
+# For today's homework write an application
+# that simulates 99 bottles of beer on the wall in code.
+puts "welcome to the 99 bottles game"
---
+++ solution.rb
@@ -0,0 +1,4 @@
+99.downto(1) do |count|
+  puts "#{count} bottles of beer on the wall, #{count} bottles of beeeeeeer!"
+end
+puts "we're all drunk!"
